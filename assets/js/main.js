b3710301c053d1a7723f51e292041678898ba539
creio <mailcreio@gmail.com>
2020-03-09 19:48:13 +0300

    2020.03.09 Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // scrolljump
-$(function() { 
+$(function() {
   $(window).scroll(function() {
     if($(this).scrollTop() != 0) {
       $('#scrollup').fadeIn();
@@ -14,18 +14,18 @@ $(function() {
 
 // magnific popup
 $('.video-link').magnificPopup({
-  type: 'iframe'  
+  type: 'iframe'
 });
 
 // search form popup
 $(document).ready(function(){
-  $('a[href="#search"]').on('click', function(event) {                    
+  $('a[href="#search"]').on('click', function(event) {
     $('#search').addClass('open');
     $('#search > form > input[type="search"]').focus();
-  });            
+  });
   $('#search, #search button.close').on('click keyup', function(event) {
     if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
       $(this).removeClass('open');
     }
-  });            
+  });
 });
