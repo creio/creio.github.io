2c5c86564f41296e020df128452a46c52a65ab97
creio <mailcreio@gmail.com>
2023-04-21 18:43:20 +0300

    21.04.2023 Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 // const config = useRuntimeConfig().public
 
-useHead({
-    script: [
-        {
-            async: true,
-            src: '/js/main.js',
-            defer: true,
-            type: 'text/javascript'
-        },
-    ],
-});
+// useHead({
+//     script: [
+//         {
+//             async: true,
+//             src: '/js/main.js',
+//             defer: true,
+//             type: 'text/javascript'
+//         },
+//     ],
+// });
 </script>
 
 <template>
