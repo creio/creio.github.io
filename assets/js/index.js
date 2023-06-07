e80b526715870c1030abbc6e53c7b149bbce10d6
creio <mailcreio@gmail.com>
2023-06-07 03:31:18 +0300

    07.06.2023 Update

--- assets/js/index.js
+++ assets/js/index.js
@@ -177,3 +177,14 @@ Source:
     }
   }
 }());
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/647f954f94cf5d49dc5c312a/1h295ug1i';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
