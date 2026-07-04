fb69aa5ff061cfaf14019d1c9ea5062e5989c4ac
creio <mailcreio@gmail.com>
2026-07-04 13:30:18 +0300

    04.07.2026 Update

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -27,10 +27,12 @@ module.exports = {
         'lazyload',
         'blur-up',
         'figcaption',
+        'lightbox-overlay',
         ...whitelister([
           './assets/scss/components/_alerts.scss',
           './assets/scss/components/_buttons.scss',
           './assets/scss/components/_code.scss',
+          // './assets/scss/components/_images.scss',
           './assets/scss/components/_syntax.scss',
           './assets/scss/components/_search.scss',
           './assets/scss/common/_dark.scss',
